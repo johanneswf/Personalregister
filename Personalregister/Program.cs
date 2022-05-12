a5c5f464747ac2afc90f3074d867c97170985346
johanneswf <johanneswf@gmail.com>
2022-05-12 12:54:31 +0200

    Cleaned up formatting. Added some input validation.

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,48 +1,70 @@
-﻿List<dynamic[]> personalregister = new List<dynamic[]>();
+﻿
+
+List<dynamic[]> personalregister = new List<dynamic[]>();
+
 bool run = true;
 while (run)
 {
     Console.Clear();
-    Console.WriteLine("Välj ett alternativ nedan:");
     Console.WriteLine("1. Skriv ut personalregistret.");
     Console.WriteLine("2. Lägg till ny personal");
     Console.WriteLine("3. Avsluta");
+    Console.WriteLine();
+    Console.Write("Val: ");
+
     switch (Console.ReadLine())
     {
         case "1":
-            Console.Clear();
             if (personalregister.Count > 0)
             {
                 Console.WriteLine("Skriv ut personalregistret\n");
                 foreach (dynamic[] personal in personalregister)
                 {
-                    Console.WriteLine(personal[0] + " " + personal[1] + ", " + personal[2] + "kr");
+                    Console.WriteLine($"Namn: {personal[0]}, Lön: {personal[1]}kr");
                 }
             }
             else
             {
-                Console.WriteLine("Det finns inga personal i personalregistret\n");
+                Console.WriteLine("Det finns inga personal i personalregistret");
             }
+
             Console.WriteLine("Tryck på Enter för att återgå till menyn.\n");
             Console.ReadLine();
+
             break;
         case "2":
-            Console.Clear();
-            Console.WriteLine("Lägg till ny personal\n");
-            Console.Write("Förnamn: ");
-            string förnamn = Console.ReadLine();
-            Console.Write("Efternamn: ");
-            string efternamn = Console.ReadLine();
-            double lön = 0;
-            while (lön == 0)
+            Console.WriteLine("\tLägg till ny personal\n");
+
+            string namn;
+            do
             {
-                Console.Write("Lön (endast siffror): ");
+                Console.Write("\nNamn: ");
+                namn = Console.ReadLine();
+                if (string.IsNullOrEmpty(namn))
+                {
+                    Console.WriteLine("Personen behöver ett namn.");
+                }
+            } while (string.IsNullOrEmpty(namn));
+
+            double lön = 0;
+            do
+            {
+                Console.Write("\nLön: ");
                 Double.TryParse(Console.ReadLine(), out lön);
-            }
-            personalregister.Add(new dynamic[] { förnamn, efternamn, lön });
+                if (lön <= 0)
+                {
+                    Console.WriteLine("Personen behöver en lön, endast nummer accepteras.");
+                }
+            } while (lön <= 0);
+
+            personalregister.Add(new dynamic[] { namn, lön });
+
             break;
         case "3":
             run = false;
+            break;
+        default:
+            Console.WriteLine("Nagivera menyn genom att trycka in den siffra som motsvarar det du vill utföra.");
             break;
     }
 }
